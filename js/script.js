6293cf2829548487e128fdcee44b85c5f9a5895a
ibdkn <ibudiakina@gmail.com>
2023-12-06 21:45:11 +0300

    added html and css

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// Data
+let playlist = {
+    title: "Rock Hits",
+    coverImageUrl: "./playlist1.png",
+    info: {
+        totalTracksCount: 4,
+        totalTracksDurationInSeconds: 733,
+    },
+    tracks: [
+        {
+            coverImageUrl: "track1.jpg",
+            artistName: "Guns N' Roses",
+            title: "Don't Cry",
+            fileUrl: "Guns_N'_Roses_Don't_Cry.mp3",
+            isHot: true,
+        },
+        {
+            coverImageUrl: "track2.jpg",
+            artistName: "Guns N' Roses",
+            title: "This I Love",
+            fileUrl: "Guns_N'_Roses_This_I_Love.mp3",
+            isHot: true,
+        },
+        {
+            coverImageUrl: "track3.jpg",
+            artistName: "Metallica",
+            title: "Master Of Puppets",
+            fileUrl: "Metallica_Master_Of_Puppets.mp3",
+            isHot: true,
+        },
+        {
+            coverImageUrl: "track3.jpg",
+            artistName: "Iron Maiden",
+            title: "Wasted Ears",
+            fileUrl: "Iron_Maiden_Wasted _ears.mp3",
+            isHot: true,
+        },
+    ]
+}
+
+// render
+
